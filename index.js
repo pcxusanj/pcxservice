b3a170a55318d485ab0e144909ef0c37cdb08da2
pcxusanj <pcxusanj@gmail.com>
2018-08-30 15:26:47 -0400

    structure

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 
 const mongoose = require('mongoose');
-
+const express = require('express');
 
 // router 
 
 
 
 // connect to mongoDB
-
 
 mongoose.connect('mongodb://localhost/pcxservice', { useNewUrlParser: true })
 .then(console.log('Connecting to MongoDB....'))
