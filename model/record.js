767454393733a9ed40a4dde95c0e5cf99eee5357
pcxusanj <pcxusanj@gmail.com>
2018-08-31 15:07:28 -0400

    add recordSchema

--- model/record.js
+++ model/record.js
@@ -1,1 +1,18 @@
-d
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const recordSchema = new mongoose.Schema({
+    techName: String,
+    techEmail: String,
+    companyName: String,
+    serviceDate: Date,
+    arrivalTime: Date,
+    leftTime: Date,
+    description: String,
+    services: [ String ],
+    isJobDone: Boolean,
+    note: String,
+    totalHour: Number,
+
+})
+
